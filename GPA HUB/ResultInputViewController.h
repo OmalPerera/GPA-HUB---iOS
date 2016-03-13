11825447731e7ef77876ac03d9f7076265baef81
OmalPerera <omalperera8@gmail.com>
2016-03-13 17:20:14 +0530

    a new row is getting added

---
+++ GPA HUB/ResultInputViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultInputViewController.h
+//  GPA HUB
+//
+//  Created by Omal Perera on 3/13/16.
+//  Copyright © 2016 omalperera.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultInputViewController : UIViewController
+
+@end
---
+++ GPA HUB/ResultInputViewController.m
@@ -0,0 +1,47 @@
+//
+//  ResultInputViewController.m
+//  GPA HUB
+//
+//  Created by Omal Perera on 3/13/16.
+//  Copyright © 2016 omalperera.com. All rights reserved.
+//
+
+#import "ResultInputViewController.h"
+
+@interface ResultInputViewController ()
+
+@end
+
+@implementation ResultInputViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)addNewRow:(id)sender {
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(10, 200, 300, 40)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.font = [UIFont systemFontOfSize:15];
+    textField.keyboardType = UIKeyboardTypeDefault;
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    textField.delegate = self;
+    [self.view addSubview:textField];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
